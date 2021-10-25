7e3d27dc0a0ca04f41b9a5e30d794df869dfddd3
fosemberg <fosemberg@gmail.com>
2021-10-25 16:37:37 +0300

    fix removing selected class from color

--- options.js
+++ options.js
@@ -7,7 +7,7 @@ const presetButtonColors = ["#3aa757", "#e8453c", "#f9bb2d", "#4688f1"];
 function handleButtonClick(event) {
   // Remove styling from the previously selected color
   let current = event.target.parentElement.querySelector(
-    `${selectedClassName}`
+    `.${selectedClassName}`
   );
   if (current && current !== event.target) {
     current.classList.remove(selectedClassName);
